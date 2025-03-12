e19d3c5ba975d041b2e76a06027302795a557f0e
caoYong123456 <130946836+caoYong123456@users.noreply.github.com>
2025-03-12 10:00:30 +0800

    长期车续费

---
+++ src/main/java/com/ej/hgj/dao/moncarren/MonCarRenOrderDaoMapper.java
@@ -0,0 +1,18 @@
+package com.ej.hgj.dao.moncarren;
+
+import com.ej.hgj.entity.moncarren.MonCarRenOrder;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Mapper
+@Component
+public interface MonCarRenOrderDaoMapper {
+
+    MonCarRenOrder getById(String id);
+
+    List<MonCarRenOrder> getList(MonCarRenOrder monCarRenOrder);
+
+
+}
